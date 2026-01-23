2dd19917e6bc792360f6a80c6c2bc798239107ff
PingouinTrash <antonin.pierre.thomas@orange.fr>
2026-01-23 15:59:32 +0100

    Index page content

--- js/script.js
+++ js/script.js
@@ -33,5 +33,28 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // ====================
-// Animation
+// Scramble effect
 // ====================
+
+const el = document.querySelector(".scramble");
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+el.addEventListener("mouseover", () => {
+  const text = el.dataset.text;
+  let i = 0;
+
+  const interval = setInterval(() => {
+    el.textContent = text
+      .split("")
+      .map((char, index) =>
+        index < i
+          ? char
+          : chars[Math.floor(Math.random() * chars.length)]
+      )
+      .join("");
+
+    i++;
+
+    if (i > text.length) clearInterval(interval);
+  }, 50);
+});
